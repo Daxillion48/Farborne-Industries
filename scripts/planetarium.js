e24505a80a043071250ab9ada407fe702834ae6a
Daxillion48 <daxillion48@gmail.com>
2024-01-05 16:59:48 +0300

    clean up & fix

--- scripts/planetarium.js
+++ scripts/planetarium.js
@@ -1,11 +1,24 @@
-Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true;
-Planets.sun.accessible = Planets.sun.alwaysUnlocked = Planets.sun.visible = true;
-Planets.gier.accessible = Planets.gier.alwaysUnlocked = Planets.gier.visible = true;
-Planets.notva.accessible = Planets.notva.alwaysUnlocked = Planets.notva.visible = true;
-Planets.verilus.accessible = Planets.verilus.alwaysUnlocked = Planets.verilus.visible = true;
+Planets.tantros.accessible = true;
+Planets.tantros.alwaysUnlocked = true;
+Planets.tantros.visible = true;
+//Planets.tantros.name = "Tantros";
 
-Planets.gier.name = "Gier";
-Planets.tantros.name = "Tantros";
-Planets.notva.name = "Notva";
-Planets.verilus.name = "Verilus";
-Planets.sun.name = "Sun";
+//Planets.sun.accessible = true;
+//Planets.sun.alwaysUnlocked = true;
+//Planets.sun.visible = true;
+//Planets.sun.name = "Sun";
+
+Planets.gier.accessible = true;
+Planets.gier.alwaysUnlocked = true;
+Planets.gier.visible = true;
+//Planets.gier.name = "Gier";
+
+Planets.notva.accessible = true;
+Planets.notva.alwaysUnlocked = true;
+Planets.notva.visible = true;
+//Planets.notva.name = "Notva";
+
+Planets.verilus.accessible = true;
+Planets.verilus.alwaysUnlocked = true;
+Planets.verilus.visible = true;
+//Planets.verilus.name = "Verilus";
