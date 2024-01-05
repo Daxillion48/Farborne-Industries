f89badb275f2b23502da8250c1ae9485b1086bff
Daxillion48 <daxillion48@gmail.com>
2024-01-05 18:22:16 +0300

    please work

--- scripts/planetarium.js
+++ scripts/planetarium.js
@@ -1,31 +1,32 @@
-Vars.ui.settings.addCategory("@farbrone-industries", Icon.effect, t =>{
-  t.checkPref("asteroids-enabled", false);
-  t.checkPref("tantros-visible", false);
-  t.checkPref("tantros-accessible", false);
-});
+Events.on(ClientLoadEvent, () => {
+  Vars.ui.settings.addCategory("@setting.farbrone-industries", Icon.effect, t =>{
+    t.checkPref("asteroids-enabled", false);
+    t.checkPref("tantros-visible", false);
+    t.checkPref("tantros-accessible", false);
+  });
 
-let asten = Core.settings.getBool("asteroids-enabled");
-let tantvis = Core.settings.getBool("tantros-visible");
-let tanten = Core.settings.getBool("tantros-enabled");
+  let asten = Core.settings.getBool("asteroids-enabled", false);
+  let tantvis = Core.settings.getBool("tantros-visible", false);
+  let tanten = Core.settings.getBool("tantros-enabled", false);
 
-if (asten) {
-  Planets.verilus.accessible = true;
-  Planets.verilus.alwaysUnlocked = true;
-  Planets.verilus.visible = true;
-  Planets.notva.accessible = true;
-  Planets.notva.alwaysUnlocked = true;
-  Planets.notva.visible = true;
-  Planets.gier.accessible = true;
-  Planets.gier.alwaysUnlocked = true;
-  Planets.gier.visible = true;
-};
-
-if (tantvis) {
-  Planets.tantros.visible = true;
-  if (tanten) {
-    Planets.tantros.accessible = true;
-    Planets.tantros.alwaysUnlocked = true;
+  if (asten) {
+    Planets.verilus.accessible = true;
+    Planets.verilus.alwaysUnlocked = true;
+    Planets.verilus.visible = true;
+    Planets.notva.accessible = true;
+    Planets.notva.alwaysUnlocked = true;
+    Planets.notva.visible = true;
+    Planets.gier.accessible = true;
+    Planets.gier.alwaysUnlocked = true;
+    Planets.gier.visible = true;
   };
-};
 
+  if (tantvis) {
+    Planets.tantros.visible = true;
+    if (tanten) {
+      Planets.tantros.accessible = true;
+      Planets.tantros.alwaysUnlocked = true;
+    };
+  };
+});
 //Thank you so much sh1penfire#0
